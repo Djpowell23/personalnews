697671ca6ca2fc76f79e65c8f11160b2fa353255
Devin Powell <dpowel11@kent.edu>
2019-11-08 18:38:07 -0700

    added article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import { Col, Row, Container } from "reactstrap";
+import styled from "styled-components";
+
+const ArticleRow = styled.div`
+  border: 1px solid purple;
+  height: 150px;
+`;
 
 class Article extends Component {
   state = {
@@ -15,9 +22,15 @@ class Article extends Component {
 
   render() {
     return (
-      <>
-        <h3>Article Component</h3>
-      </>
+      <ArticleRow>
+        <Row>
+          <Col md="2">img</Col>
+          <Col md="10">
+            <h3>News Article blah blah blah</h3>
+            <h6>News article description blah blah</h6>
+          </Col>
+        </Row>
+      </ArticleRow>
     );
   }
 }
